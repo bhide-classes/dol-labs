e64cb78f0b5ddc35b0f125fe4578bf730160e374
bhide-classes <117715683+bhide-classes@users.noreply.github.com>
2022-11-09 00:44:02 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("FirstName = "+args[0]);
+        System.out.println("Above 18 = "+args[1]);
+    }
+}
